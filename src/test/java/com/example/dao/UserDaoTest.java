c7df9a23f7ef78c8befd236bf635998bda70b25f
LipingSun <liping.sun@sjsu.edu>
2015-11-18 18:39:38 -0800

    unit test UserDao

--- src/test/java/com/example/dao/UserDaoTest.java
+++ src/test/java/com/example/dao/UserDaoTest.java
@@ -7,11 +7,10 @@ import org.junit.Test;
  * Created by Liping on 11/18/15.
  */
 public class UserDaoTest {
-    UserDao userDao;
+    UserDao userDao = new UserDaoImpl();
 
     @Test
     public void testFindUserById() throws Exception {
         Assert.assertEquals(userDao.findUserById("1"), "1");
-
     }
 }
