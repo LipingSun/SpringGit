d5ef882939f0839e2c370deae7780e9a3d325a04
Liping <Liping@MacBook-Pro.local>
2015-11-19 22:16:22 -0800

    test conflict

--- src/main/java/com/example/controller/UserController.java
+++ src/main/java/com/example/controller/UserController.java
@@ -1,6 +1,5 @@
 package com.example.controller;
 
-import com.example.entity.User;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
